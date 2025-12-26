cc8382d83fb3d1563ad532355b0b8612de3d4954
Desalegn Tamirat <desalegntamirateshetu@gmail.com>
2025-12-26 10:39:30 +0300

    prepare api/chat for the chatbot

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.post('/api/chat', async (req, res) => {
+  res.send("no answer for now")
+})
+
 app.listen(port, () => {
   console.log(`running on port http://localhost:${port}`);
 });
