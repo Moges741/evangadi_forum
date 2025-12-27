cecdc2fe03ce260dd7d2322a1e973255f57ffe1b
NJ <nej9633@gmail.com>
2025-12-27 21:13:20 +0300

    more validation

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,3 +1,7 @@
+import { StatusCodes } from "http-status-codes";
+// import dbconnection from "../db/dbconfig.js";
+
+
 const login = async (req, res) => {
   const { email, password } = req.body;
   //validate request
@@ -8,7 +12,20 @@ const login = async (req, res) => {
       .json({ msg: "Please provide all required values" });
   }
 try {
-  
+  const [users] = await dbconnection.query(
+    "select username, email, userid, password, firstname, lastname from users where email = ? ",
+    [email]
+  );
+
+  //if no user found
+  if (users.length === 0) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ msg: "Invalid Credentials" });
+  }
+const user = users[0];
+
+
 } catch (error) {
   console.log("Login error:", error.message);
   return res
