4dbe7e932578ffc4f31502c304c629af5266d7de
Moges <mogesse741@gmial.com>
2026-01-02 09:23:14 +0300

    updated  vite-config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5500',  //our backend server
+        changeOrigin: true,               // Changes origin header to match target
+        secure: false,                    // For localhost
+      },
+    },
+  },
+});
