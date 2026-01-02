b4190d1138168dad9a5e7ef2f033df6467d5f3e7
mifta-kedir <49817419+mifta-kedir@users.noreply.github.com>
2026-01-02 09:13:25 -0500

    App.jsx updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,47 @@
-import { useState } from 'react'
-import Layout from './components/Layout/Layout'
+import { useEffect, useState, createContext } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Login from "./Pages/Login/Login";
+import axios from "./Api/axiosConfig";
 
 function App() {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+
+  async function checkUser() {
+    if (!token) {
+      navigate("/login");
+      return;
+    }
+
+    try {
+      await axios.get("/users/check", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (error) {
+      console.error(error.response);
+      localStorage.removeItem("token");
+      navigate("/login");
+    }
+  }
+
+  useEffect(() => {
+    checkUser();
+  }, []);
 
   return (
-    <>
-    <Layout/>
-    </>
-  )
+    <div>
+      <Routes>
+        {/* Public Route */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Protected Routes */}
+        <Route path="/*" element={<Layout />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
