e76c36660472ba407f699306adda6f5379ca586c
Moges <mogesse741@gmial.com>
2026-01-03 21:18:42 +0300

    switched to main

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "../Header/Header"; // Adjust path if needed
-
+import Register from "../../Pages/Register/Register";
 function Layout() {
   return (
     <>
