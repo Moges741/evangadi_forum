961cf55df7dcd428295787b5ad4d0e4a08ad467e
Moges <mogesse741@gmial.com>
2026-01-11 21:45:14 +0300

    yoooo

--- server/DB/dbconfig.js
+++ server/DB/dbconfig.js
@@ -1,12 +1,26 @@
+// import mysql from "mysql2/promise";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const dbconnection = mysql.createPool({
+//   host: "localhost",
+//   user: "forum@User",
+//   password: "Forum@User123!",
+//   database: "FORUM_DB",
+//   connectionLimit: 11,
+// });
+
+// export default dbconnection;
+
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
 const dbconnection = mysql.createPool({
-  host: "localhost",
-  user: "forum@User",
-  password: "Forum@User123!",
-  database: "FORUM_DB",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   connectionLimit: 11,
 });
 
