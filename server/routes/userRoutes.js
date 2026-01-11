0277f6d00920c82790ce7fb5e0a86a506b76bcd3
nejat <nej9633@gmail.com>
2026-01-11 16:33:32 +0300

    forgot and reset password added both backend and frontend

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,19 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import { login, register, checkUser } from "../controller/userController.js";
+import {
+  login,
+  register,
+  checkUser,
+  forgotPassword,
+  resetPassword,
+} from "../controller/userController.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.get("/check", authMiddleware, checkUser);
 router.post("/register", register);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
