e2c197966ce978d887185195a94311c2701a1dbf
Moges <mogesse741@gmial.com>
2026-01-15 12:23:29 +0300

    added

--- client/src/Data/data.js
+++ client/src/Data/data.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://evangadiforum.be.birhann.com";
+const API_BASE_URL = "https://evangadi-forum-1-edti.onrender.com";
 // const API_BASE_URL = "http://localhost:5500";
 
 export { API_BASE_URL };
